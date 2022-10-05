9ea4c07c58727474542624de7035c8ac39112f59
Alexandra <matildakrokodilda@gmail.com>
2022-10-05 11:20:05 +0300

    задача 13

---
+++ sem2_013/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// // Задача 13: Напишите программу, которая выводит третью цифру СЛЕВА заданного числа или сообщает, что третьей цифры нет.
+// // 645 -> 5
+// // 78 -> третьей цифры нет
+// // 3267912 -> 6
+
+
+string q = Convert.ToString(new Random().Next());
+Console.WriteLine(q);
+//Console.WriteLine(q.Length);
+if (q.Length>3)
+{
+Console.WriteLine(q[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
