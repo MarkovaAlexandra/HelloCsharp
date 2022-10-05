c36d2ba78bf6c7239d8d6429a0486310748dfca0
Alexandra <matildakrokodilda@gmail.com>
2022-10-05 15:37:31 +0300

    задача 13 через цикл

--- sem2_013/Program.cs
+++ sem2_013/Program.cs
@@ -5,15 +5,53 @@
 // // 3267912 -> 6
 
 
-string q = Convert.ToString(new Random().Next());
-Console.WriteLine(q);
-//Console.WriteLine(q.Length);
-if (q.Length>3)
-{
-Console.WriteLine(q[2]);
-}
-else
-{
+// string q = Convert.ToString(new Random().Next());
+// Console.WriteLine(q);
+// //Console.WriteLine(q.Length);
+// if (q.Length>3)
+// {
+// Console.WriteLine(q[2]);
+// }
+// else
+// {
+//     Console.WriteLine("третьей цифры нет");
+// }
+
+int num = new Random().Next(0,2000000000);
+///int num = 1424618595;
+Console.WriteLine($"случайное число {num}");
+
+
+if (num<100)
+{ 
     Console.WriteLine("третьей цифры нет");
+}
+int result = 0;// третья цифра
+int s=1000;//будем сравнивать рандомное число с этим
+int a =10;//будем искать остаток от деления на а
+int b=1;//будем делить остаток на b
+int result1=0;// первый промежуточный рез
+while(num<int.MaxValue)
+{
+if (num<s)
+{
+    Console.WriteLine($"строка 38   {num} {s}");
+    result1=num%a;
+    Console.WriteLine($"строка 40    {result1} = {num}  {a}  ");
+    if (result1>b)
+    {
+        Console.WriteLine($"строка 43   {result1} {b}");
+        result=result1/b;
+    }
+    if (result1<b)
+    {
+        result = 0;
+    }
+    Console.WriteLine(result);
+    break;
+}
+a=a*10;
+b=b*10;
+s=s*10;
 }
 
