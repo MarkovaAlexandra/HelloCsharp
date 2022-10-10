ba92c8a8b1b467cff3b85fed1fd7bdd0bbcd9000
Alexandra <matildakrokodilda@gmail.com>
2022-10-10 13:04:14 +0300

    поэлегантнее решение

---
+++ sem2_3cifra_sleva/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вывести третью цифру слева
+int num = new Random().Next(0,int.MaxValue);
+Console.WriteLine($"random = {num}");
+if (num<100) Console.WriteLine("третьей цифры нет");
+long i =1000;
+int result = 0;
+int delitel = 1;
+while (num<int.MaxValue)
+{
+    if(num<i)
+    {
+        result=num/delitel%10;
+        Console.WriteLine(result);
+        break;
+    }
+    i=i*10;
+    delitel=delitel*10;
+}
