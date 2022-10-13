514a6c4e3c2d24f6e4a77233abeb857230109b45
Alexandra <matildakrokodilda@gmail.com>
2022-10-13 09:50:32 +0300

    задача 25, 27

---
+++ sem4_25/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int Stepen(int x, int y)
+{
+    int result=x;
+    for(result=x; y>1; y--)
+    {
+        result=result*x;
+    }
+    return result;
+}
+     
+//     int result=x;
+//     while(y>1)
+//     {
+//         result = result*x;
+//         Console.WriteLine(result);
+//         y--;
+//         Console.WriteLine(y);
+//     }
+//     return result;
+// }
+ int d = Stepen(3,5);
+ Console.WriteLine(d);
+ int f= Stepen(8,3);
+ Console.WriteLine(f);
+ 
---
+++ sem4_27/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumCifr(int x)
+{
+    if (x<0) x=x*-1;
+    int count=1;
+    int edinicVRazriade = 0;
+    int sum=0;
+    // while(x>0)
+    // { 
+    //     edinicVRazriade=x%10;
+    //     x=x/10;
+    //     count++;
+    //     //Console.WriteLine($"{edinicVRazriade}");
+    //     sum=sum+edinicVRazriade;
+    // }
+    for(sum=0; x>0; count++)
+    {
+        edinicVRazriade=x%10; 
+        x=x/10;sum=sum+edinicVRazriade;
+    }
+    return sum;
+}
+
+int t = new Random().Next();
+Console.WriteLine(t);
+int m=SumCifr(t);
+Console.WriteLine(m);
