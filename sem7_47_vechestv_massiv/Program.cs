0d9518eb19b8f18bd2408d1fb021dc0ef90d3a24
Alexandra <matildakrokodilda@gmail.com>
2022-10-24 14:11:19 +0300

    задача 47

---
+++ sem7_47_vechestv_massiv/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+void FillArr (double [,] Array)
+{
+   for (int i = 0; i < Array.GetLength(0); i++)
+   {
+     for (int j = 0; j < Array.GetLength(1); j++)
+     {
+        Array[i,j] = Math.Round((new Random().NextDouble() * 10),2);
+     }
+   }
+}
+void PrintAr(double[,]Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+     for (int j=0; j<Array.GetLength(1); j++)
+     {
+        Console.Write ($"{Array[i,j]}\t");
+        
+     }  
+     System.Console.WriteLine(); 
+    }
+}
+double[,]Massiv = new double[6,3];
+FillArr(Massiv);
+PrintAr(Massiv);
