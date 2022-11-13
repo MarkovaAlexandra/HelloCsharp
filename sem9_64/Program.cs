3177d22437b9e52ad6c90c197ec52cb4b6fa590e
Alexandra <matildakrokodilda@gmail.com>
2022-11-13 16:48:11 +0300

    семинар 9

---
+++ sem9_64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+System.Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+void NumsToN(int N)
+{
+    System.Console.Write($"{N} ");
+    if (N==1) return;
+    N--;
+    NumsToN(N);
+}
+NumsToN(n);
+
+
---
+++ sem9_66_Sum_MtoN_recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNums (int m, int n)
+{
+    if(n==m+1) return(m+n);
+    else return (n+SumNums(m,n-1));
+}
+System.Console.WriteLine("Введите число M");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число N");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    int temp=0;
+    temp=a;
+    a=b;
+    b=temp;
+}
+int sum = SumNums(a,b);
+System.Console.WriteLine(sum);
